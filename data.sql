54c179a8c74e41bd314b6ad95fb36a87ed4298d0
vikklimenko <viktoriya-klimenko2003@mail.ru>
2022-01-29 20:55:48 +0300

    13-14

--- data.sql
+++ data.sql
@@ -1,1 +1,3 @@
-select * from group_member
+select m.first_name, m.middle_name, m.last_name, m.e_mail, d.name 
+from university_member um join tbl_1 m on um.user_id=m.id 
+join department d on um.department_id=d.id;
